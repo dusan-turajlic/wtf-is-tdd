7fdc1da32a9ac713985efa9a773475dcb46bf708
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2023-03-17 09:32:41 +0200

    Fixes startup error and removes needless properties (#5)

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import http from 'http';
 import { appConfig } from './config';
 import { app } from './server';
 
-const { env, name, version, host, port } = appConfig;
+const { env, name, host, port } = appConfig;
 
 const httpServer = http.createServer(app.callback());
 
 httpServer.listen({ host, port }, () => {
   // eslint-disable-next-line no-console
-  console.info(`${name}@${version} server listening on ${host}:${port}, in ${env}`);
+  console.info(`${name} server listening on ${host}:${port}, in ${env}`);
 });
