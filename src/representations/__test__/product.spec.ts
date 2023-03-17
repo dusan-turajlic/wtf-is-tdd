2143230918f356cdd3e1e0401e8f3e927d27c349
Dusan Turajlic <dusan@turajlic.fi>
2023-03-17 20:26:31 +0200

    RED

--- src/representations/__test__/product.spec.ts
+++ src/representations/__test__/product.spec.ts
@@ -1,3 +1,37 @@
+import { Product } from '../index';
+
 describe('Product', () => {
-  it('cannot pass values that', () => {});
+  const basicProduct = {
+    productId: '123',
+    name: 'Product name',
+    amount: 4,
+    price: 2999,
+    images: ['https://i.imgur.com/oXRmvsF.jpeg'],
+  };
+  it('Does not throw when parsing', () => {
+    expect(() => {
+      Product.parse({
+        ...basicProduct,
+      });
+    }).not.toThrow();
+  });
+
+  it('Maps price correctly', () => {
+    const parsed = Product.parse({
+      ...basicProduct,
+    });
+
+    expect(parsed.price).toBe(29.99);
+  });
+
+  it.each([
+    [{ ...basicProduct, productId: 1 }],
+    [{ ...basicProduct, name: {} }],
+    [{ ...basicProduct, price: 22.99 }],
+    [{ ...basicProduct, images: null }],
+  ])('throws when odd values are given', params => {
+    expect(() => {
+      Product.parse(params);
+    }).toThrow();
+  });
 });
