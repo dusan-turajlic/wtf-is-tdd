613aa490f017c856043688c4b7fdbff3898beedb
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2023-03-22 01:00:39 +0200

    Adds react router (#12)

--- frotnend/src/main.tsx
+++ frotnend/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { ProductView } from './views/product';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 const root = document.getElementById('root') as HTMLElement;
 createRoot(root).render(
   <React.StrictMode>
-    <ProductView />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
